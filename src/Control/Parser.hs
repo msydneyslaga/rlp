b28daefc3c3b69e755343146c325458d6ea7b785
crumbtoo <crumb@disroot.org>
2023-11-15 14:31:27 -0700

    lexer

--- src/Control/Parser.hs
+++ src/Control/Parser.hs
@@ -1,6 +1,6 @@
 {-|
 Module      : Control.Parser
-Description : The parser *object*
+Description : Parser combinators
 
 This module implements an interface for parser *types*, used in lexical analysis
 and parsing. For the implementation of the rlp language's parser, see 'Parse'.
@@ -8,13 +8,27 @@ and parsing. For the implementation of the rlp language's parser, see 'Parse'.
 {-# LANGUAGE GeneralisedNewtypeDeriving #-}
 {-# LANGUAGE BlockArguments, LambdaCase #-}
 module Control.Parser
-    (
+    ( ParserT
+    , runParserT
+
+    , satisfy
+    , char
+    , spaces
+    , nl
+    , surround
+    , string
+
+    -- * Control.Applicative re-exports
+    , (<|>)
+    , many
+    , some
     )
     where
 ----------------------------------------------------------------------------------
 import Control.Applicative
 import Control.Arrow                ((***))
 import Control.Monad
+import Data.Char
 ----------------------------------------------------------------------------------
 
 newtype ParserT i m o = ParserT { runParserT :: i -> m (i, o) }
@@ -40,11 +54,35 @@ instance (Monad m) => Monad (ParserT i m) where
         (i',a) <- runParserT m i
         runParserT (k a) i'
 
+instance (MonadFail m) => MonadFail (ParserT i m) where
+    fail s = ParserT $ \i -> fail s
 ----------------------------------------------------------------------------------
 
 -- TODO: generalise to non-lists
-satisfy :: (MonadPlus m, Eq a) => a -> ParserT [a] m a
-satisfy c = ParserT $ \case
-    (x:xs) | x == c -> pure (xs,x)
-    _               -> empty
+satisfy :: (MonadPlus m) => (a -> Bool) -> ParserT [a] m a
+satisfy p = ParserT $ \case
+    (x:xs) | p x -> pure (xs,x)
+    _            -> empty
+
+
+char :: (MonadPlus m, Eq a) => a -> ParserT [a] m a
+char c = satisfy (==c)
+
+string :: (MonadPlus m, Eq a) => [a] -> ParserT [a] m [a]
+string s = sequenceA $ char <$> s
+
+----------------------------------------------------------------------------------
+
+surround :: (MonadPlus m)
+         => ParserT i m l
+         -> ParserT i m r
+         -> ParserT i m c
+         -> ParserT i m c
+surround l r c = l *> c <* r
+
+spaces :: (MonadPlus m) => ParserT String m Int
+spaces = length <$> many (satisfy (==' '))
+
+nl :: (MonadPlus m) => ParserT String m Int
+nl = undefined
 
--- src/Core/Parse.hs
+++ src/Core/Parse.hs
@@ -3,4 +3,11 @@ module Core.Parse
     )
     where
 ----------------------------------------------------------------------------------
+import Control.Parser
+import Core.Lex
+import Core.Syntax
+----------------------------------------------------------------------------------
+
+parseCore :: [CoreToken] -> Result Program
+parseCore = undefined
 
