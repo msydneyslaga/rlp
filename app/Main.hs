b31c50dd72a8e5e2d424c6d8998143eafa03b996
crumbtoo <crumb@disroot.org>
2023-12-11 17:30:32 -0700

    update old name

--- app/Main.hs
+++ app/Main.hs
@@ -133,7 +133,7 @@ ddumpEval = whenFlag flagDDumpEval do
 
 parseProg :: RLPCOptions
           -> String
-          -> Either SrcError (CoreProgram, [SrcError])
+          -> Either SrcError (Program', [SrcError])
 parseProg o = evalRLPC o . (lexCore >=> parseCoreProg)
 
 forFiles_ :: (Monad m)
