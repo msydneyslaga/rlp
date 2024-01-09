936f24148f1268eb21a5c9190272679e82b4ac75
crumbtoo <crumb@disroot.org>
2024-01-08 18:56:14 -0700

    works

--- src/RLP/Parse/Types.hs
+++ src/RLP/Parse/Types.hs
@@ -1,9 +1,17 @@
+{-# LANGUAGE ImplicitParams, ViewPatterns, PatternSynonyms #-}
+{-
+Description : Supporting types for the parser
+-}
 module Rlp.Parse.Types
     (
     -- * Partial ASTs
       Partial(..)
+    , PartialE
     , PartialExpr'
     , PartialDecl'
+    , pattern WithInfo
+    , pR
+    , pL
 
     -- * Parser types
     , Parser
@@ -20,7 +28,9 @@ import Data.Functor.Foldable
 import Data.Functor.Const
 import Data.Functor.Classes
 import Data.Void
+import Data.Maybe
 import Text.Megaparsec              hiding (State)
+import Lens.Micro
 import Rlp.Syntax
 ----------------------------------------------------------------------------------
 
@@ -46,6 +56,23 @@ data Partial a = E (RlpExprF Name a)
                | B Name (Partial a) (Partial a)
                | P (Partial a)
                deriving (Show, Functor)
+
+pL :: Traversal' (Partial a) (Partial a)
+pL k (B o l r) = (\l' -> B o l' r) <$> k l
+pL _ x         = pure x
+
+pR :: Traversal' (Partial a) (Partial a)
+pR k (B o l r) = (\r' -> B o l r') <$> k r
+pR _ x         = pure x
+
+type PartialE = Partial RlpExpr'
+
+-- i love you haskell
+pattern WithInfo :: (?pt :: OpTable) => OpInfo -> PartialE -> PartialE -> PartialE
+pattern WithInfo p l r <- B (opInfoOrDef -> p) l r
+
+opInfoOrDef :: (?pt :: OpTable) => Name -> OpInfo
+opInfoOrDef c = fromMaybe (InfixL,9) $ H.lookup c ?pt
 
 -- required to satisfy constraint on Fix's show instance
 instance Show1 Partial where
